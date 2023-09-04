acabbc2435a4c1ccd5052120ea4d2c3b28d80819
Acer <dmv053@gmail.com>
2023-09-05 00:05:55 +0400

    реализован вывод заданных значений

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
-import json
-import datetime
-from functions import load_file, created_list
+from functions import load_file, created_list, output_mask
 
-FILE_NAME = 'operations.json'
+FILE_NAME = 'operations.json' # путь к фалу с данными
 
-list = load_file(FILE_NAME)
 
-normal_list = created_list(list)
+list_ = load_file(FILE_NAME)
+normal_list = created_list(list_)
 normal_list = sorted(normal_list, key=lambda x: x['date'], reverse=True)
+for i in range(5):
+    print(f'{normal_list[i]["date"].strftime("%d.%m.%Y")} {normal_list[i]["description"]}')
+    print(f'{output_mask(normal_list[i]["from"])} -> {output_mask(normal_list[i]["to"])}')
+    print(f'{normal_list[i]["amount"]} {normal_list[i]["currency"]}\n')

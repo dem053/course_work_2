69181077031e1010440b8c634a6cc12886a68836
Acer <dmv053@gmail.com>
2023-09-05 00:04:48 +0400

    реализована функция output_mask возвращающая вывод номера/карты по заданной маске

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,5 @@
-import json
 import datetime
-
+import json
 
 def load_file(file_name):
     """Достает данные из файла json"""
@@ -11,7 +10,7 @@ def load_file(file_name):
 def created_list (list):
     normal_list=[]
     for i in list:
-        if len(i) !=0:
+        if len(i) !=0 and i['state'] == "EXECUTED":
             dic = {}
             dic['date'] = datetime.datetime.strptime(i['date'], '%Y-%m-%dT%H:%M:%S.%f')
             dic['description'] = i['description']
@@ -26,3 +25,17 @@ def created_list (list):
             normal_list.append(dic)
     return normal_list
 
+def output_mask (str_in):
+    if str_in == None:
+        return ''
+    else:
+        count_int = len ([sym for sym in str_in if sym.isdigit()])
+        str_in = str_in.strip()
+        str_in = ''.join(reversed(str_in))
+        if count_int == 16:
+            number = str_in[0:4]+' ****'+' **'+str_in[10:12]+' '+str_in[12:16]
+            name = str_in[16:]
+        else:
+            number = str_in[0:4]+'**'
+            name = str_in[20:]
+    return ''.join(reversed(name))+''.join(reversed(number))
